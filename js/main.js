97db38872c0aa85047c8dbdf7b91e7ed589490a3
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-07-18 20:41:08 -0400

    basic file/folder struct

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const headerGlobal = document.querySelector('#header-global');
+
+headerGlobal.innerHTML = `
+
+    <ul class="header-navigation-ul">
+        <li class="header-navigation-li"><a href="index.html">Home</a></li>
+        <li class="header-navigation-li"><a href="biography.html">Biography</a></li>
+        <li class="header-navigation-li"><a href="tutorial.html">Tutorial</a></li>
+        <li class="header-navigation-li"><a href="sectionfour.html">Section Four</a></li>
+    </ul>`;
