d26a236694c69f696f564c3b3a3250bea23a10a9
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-07-26 03:54:51 -0400

    delete js; using php instead for head, header footer

---
+++ templates/header.php
@@ -0,0 +1,11 @@
+<header id="header">
+    <nav id="nav">
+        <p id="header-logo">Logo Placeholder</p>
+        <ul id="header-navigation-ul">
+            <li><a href="../index.html">Home</a></li>
+            <li><a href="../biography.html">About</a></li>
+            <li><a href="../tutorial.html">Tutorials</a></li>
+            <li><a href="../register.php">Register</a></li>
+        </ul>
+    </nav>
+</header>
