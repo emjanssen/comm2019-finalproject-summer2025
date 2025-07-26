8067de302865df2abfdfe8bc4ac9861040ed49fc
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-07-26 03:45:39 -0400

    edits to header

--- js/main.js
+++ js/main.js
@@ -2,9 +2,22 @@ const headerGlobal = document.querySelector('#header-global');
 
 headerGlobal.innerHTML = `
 
-    <ul class="header-navigation-ul">
-        <li class="header-navigation-li"><a href="index.html">Home</a></li>
-        <li class="header-navigation-li"><a href="biography.html">Biography</a></li>
-        <li class="header-navigation-li"><a href="tutorial.html">Tutorial</a></li>
-        <li class="header-navigation-li"><a href="animations.html">Section Four</a></li>
-    </ul>`;
+<header id="header-global">
+
+            <div id="header-logo">
+                <img id="img-logo" src="" alt="Page logo.">
+            </div>
+
+            <div id="header-title">
+                <h1>CSS Animations</h1>
+            </div>
+
+            <nav id="header-nav">
+                <ul id="header-nav-ul">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="biography.html">Biography</a></li>
+                    <li><a href="tutorial.html">Tutorial</a></li>
+                    <li><a href="animations.html">Animations</a></li>
+                </ul>
+            </nav>
+`;
