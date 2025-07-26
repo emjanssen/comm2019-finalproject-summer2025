925ce81466c1f7c7d325dfddb55f296b1d78a494
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-07-26 04:00:48 -0400

    initial php files

--- templates/header.php
+++ templates/header.php
@@ -3,9 +3,9 @@
         <p id="header-logo">Logo Placeholder</p>
         <ul id="header-navigation-ul">
             <li><a href="../index.html">Home</a></li>
-            <li><a href="../biography.html">About</a></li>
-            <li><a href="../tutorial.html">Tutorials</a></li>
-            <li><a href="../register.php">Register</a></li>
+            <li><a href="../biography.html">Biography</a></li>
+            <li><a href="../tutorial.html">Tutorial</a></li>
+            <li><a href="../animations.html">Animations</a></li>
         </ul>
     </nav>
 </header>
