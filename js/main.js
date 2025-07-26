5add64c4eb303d0ceee962c8aee5356d2883801d
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-07-26 06:25:31 -0400

    edits to main.js, changed header layout to grid

--- js/main.js
+++ js/main.js
@@ -1,24 +1,44 @@
+if (typeof pageTitle !== 'undefined') {
+  document.title = `Placeholder - ${pageTitle}`;
+} else {
+  document.title = `Placeholder - Untitled`;
+}
+
 const headerGlobal = document.querySelector('#header-global');
-headerGlobal.innerHTML = `
-  <div id="header-logo">
-    <img id="img-logo" src="" alt="Page logo.">
-  </div>
-  <div id="header-title">
-    <h1>${pageTitle}</h1>
-  </div>
-  <nav id="header-nav">
-    <ul id="header-navigation-ul">
-      <li><a href="index.html">Home</a></li>
-      <li><a href="biography.html">Biography</a></li>
-      <li><a href="tutorial.html">Tutorial</a></li>
-      <li><a href="animations.html">Animations</a></li>
-    </ul>
-  </nav>
-`;
+if (headerGlobal) {
+  headerGlobal.innerHTML = `
+    <div id="header-global-grid">
+
+      <div id="header-global-grid-col-one">
+        <div id="header-logo">
+          <img id="img-logo" src="css/img/headerLogo.png" alt="Page logo.">
+        </div>
+      </div>
+
+      <div id="header-global-grid-col-two">
+        <div id="header-title">
+          <h1>${pageTitle || 'Untitled'}</h1>
+        </div>
+      </div>
+
+      <div id="header-global-grid-col-three">
+        <nav id="header-nav">
+          <ul id="header-navigation-ul">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="biography.html">Biography</a></li>
+            <li><a href="tutorial.html">Tutorial</a></li>
+            <li><a href="animations.html">Animations</a></li>
+          </ul>
+        </nav>
+      </div>
+
+    </div>
+  `;
+}
 
 const footerGlobal = document.querySelector('#footer-global');
-footerGlobal.innerHTML = `
-  <p>&copy; 2025 Elizabeth Janssen. All rights reserved.</p>
-`;
-
-document.title = `Placeholder - ${pageTitle}`;
+if (footerGlobal) {
+  footerGlobal.innerHTML = `
+    <p>&copy; 2025 Elizabeth Janssen. All rights reserved.</p>
+  `;
+}
